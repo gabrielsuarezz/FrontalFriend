e207eb2ef00c25a59c737037f544bdb1e4835ab1
Gabriel Suarez <gsuar092@fiu.edu>
2026-01-21 14:47:44 -0500

    Configure base URL for Expo Router on GitHub Pages

--- FrontalFriend/metro.config.js
+++ FrontalFriend/metro.config.js
@@ -2,4 +2,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
 module.exports = config;
